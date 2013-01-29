11aa1b9cfbb308390fc777b4d88be43ac25d0747
Dominik Liebler <liebler.dominik@googlemail.com>
2013-01-29 14:25:21 +0100

    initial commit

---
+++ DomniklStatsdBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the DomniklStatsdBundle.
+ *
+ * (c) Dominik Liebler <liebler.dominik@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Domnikl\StatsdBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Dominik Liebler <liebler.dominik@googlemail.com>
+ *
+ */
+class DomniklStatsdBundle extends Bundle
+{
+
+}
---
+++ Service/Client.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Domnikl\StatsdBundle\Service;
+
+final class Client
+{
+    /**
+     * @var \Domnikl\Statsd\Client
+     */
+    private $client;
+
+    /**
+     *
+     *
+     * @param string $adapterClass
+     * @param string $namespace
+     * @param array $connectionOptions
+     */
+    public function __construct($adapterClass, $namespace = '', array $connectionOptions = array())
+    {
+        $host = $connectionOptions['host'];
+        $port = $connectionOptions['port'];
+
+        $connection = new $adapterClass($host, $port);
+        $this->client = new \Domnikl\Statsd\Client($connection, $namespace);
+    }
+
+    /**
+     * @return \Domnikl\Statsd\Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * route calls to statsd client
+     *
+     * @param string $name
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function __call($name, $params)
+    {
+        return call_user_func_array(array($this->client, $name), $params);
+    }
+}
