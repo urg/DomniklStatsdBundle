fe2e6c0f0f39fdf40fab00c389d234c4f426726c
Dominik Liebler <liebler.dominik@googlemail.com>
2014-01-27 18:20:21 +0100

    cleanup

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -40,10 +40,6 @@ class Configuration implements ConfigurationInterface
                         ->defaultValue(8125)
                     ->end();
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
         return $treeBuilder;
     }
 }
